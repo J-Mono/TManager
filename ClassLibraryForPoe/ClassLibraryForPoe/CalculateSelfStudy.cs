1a423c983685fe11dc3ae0c528a32e19eaa7ad76
JosephNtsekalle <79351812+J-Mono@users.noreply.github.com>
2023-04-03 01:31:31 +0200

    Add files via upload

---
+++ ClassLibraryForPoe/ClassLibraryForPoe/CalculateSelfStudy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryForPoe
+{
+    public class CalculateSelfStudy
+    {
+
+        //method to calculate the study hours weekly
+        public int calcStudyHours(string Credits, int Nr_of_weeks, string class_hp)
+        {
+            //weekly study hours = credits time 10. 
+            //then that ansswer is divided by the number of weeks
+            //then that answer is subtracted by the class hours
+            int credits = Convert.ToInt32(Credits);
+            int classhours = Convert.ToInt32(class_hp);
+
+            return(credits * 10 / Nr_of_weeks) - classhours;
+        }
+    }
+}
